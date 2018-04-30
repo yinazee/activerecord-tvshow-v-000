f6fa9c316c0e5374617e6d7b841008e3d047a325
Yina Zhu <yinagit@gmail.com>
2018-04-30 21:30:05 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,11 @@
 class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    highest_rating.name
+  end
+
 end
--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,6 +1,7 @@
 class CreateShows < ActiveRecord::Migration
 
   def change
+
     create_table :shows do |t|
       t.string :name
       t.string :network
