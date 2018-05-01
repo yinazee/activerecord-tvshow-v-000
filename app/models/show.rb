27b4384ba4198d7c498b0feaeda39e51ee365d5c
Yina Zhu <yinagit@gmail.com>
2018-05-01 02:31:30 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -14,7 +14,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    Show.where(rating: self.lowest_rating)first
+    Show.where(rating: self.lowest_rating).first
   end
 
 end
